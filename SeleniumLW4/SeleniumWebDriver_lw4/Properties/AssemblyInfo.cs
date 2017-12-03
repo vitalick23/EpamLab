d3d48875ac8bf71221f20ad775d5e04fdd4c1b56
Vitaliy <vitali_fc_arsenal@mail.ru>
2017-12-03 13:00:21 +0300

    LW4

---
+++ SeleniumLW4/SeleniumWebDriver_lw4/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWebDriver_lw4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumWebDriver_lw4")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a4ff978-12c3-4d21-9f46-00eb4fe5d41e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumLW4/SeleniumWebDriver_lw4/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumLW4
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            FirefoxDriver firefoxDriver = new FirefoxDriver();
+            firefoxDriver.Navigate().GoToUrl("https://github.com/");
+            firefoxDriver.FindElementByXPath(".//a[text() = 'Sign in']").Click();
+            firefoxDriver.FindElementById("login_field").SendKeys("vitali_fc_arsenal@mail.ru");
+            firefoxDriver.FindElementById("password").SendKeys("****");
+            firefoxDriver.FindElementByXPath("/html/body/div[3]/div[1]/div/form/div[4]/input[3]").Click();
+
+        }
+    }
+}
